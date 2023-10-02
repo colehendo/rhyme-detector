b80661c61c96b9deeed26a5468baa5a15f793166
Cole Henderson <cole.hendo.tech@gmail.com>
2023-10-02 08:14:54 -0700

    organized code and removed grayscale colors

---
+++ printer.py
@@ -0,0 +1,61 @@
+from functools import cache
+from random import randint
+from typing import List, Tuple
+
+
+class Printer:
+    def __init__(self) -> None:
+        pass
+
+    @property
+    @cache
+    def color_codes_to_avoid(self) -> List[int]:
+        black = 0
+        white = 15
+        gray = 8
+
+        grayscale = list(range(232, 256))
+        grayscale.extend([black, white, gray])
+
+        return grayscale
+
+    def pick_color_code(self, selected_numbers: List[int]) -> int:
+        number = randint(1, 231)
+
+        # The last conditional checks if there are still unique ascii values
+        if number in self.color_codes_to_avoid or (number in selected_numbers and len(selected_numbers) < 236):
+            self.pick_color_code(selected_numbers)
+
+        selected_numbers.append(number)
+        return number
+
+    def color_word_backgrounds(
+        self, lines: List[str], matched_word_placements: List[List[Tuple[int]]]
+    ):
+        color_base = "\u001b[48;5;"
+        clear = "\u001b[49m"
+
+        selected_numbers = []
+
+        for rhyme_scheme in matched_word_placements:
+            color_code = self.pick_color_code(selected_numbers)
+
+            for word_placement in rhyme_scheme:
+                line = word_placement[0]
+                word_index = word_placement[1]
+
+                lines[line][
+                    word_index
+                ] = f"{color_base}{str(color_code)}m{lines[line][word_index]}{clear}"
+
+    def print_rhyme(self, file_lines: List[str]):
+        for index, line in enumerate(file_lines):
+            file_lines[index] = " ".join(line)
+            print(file_lines[index])
+            print()
+
+    def print_rhyme_handler(
+        self, file_lines: List[str], matched_word_placements: List[List[Tuple[int]]]
+    ):
+        self.color_word_backgrounds(file_lines, matched_word_placements)
+        self.print_rhyme(file_lines)
---
+++ rhyme_builder.py
@@ -0,0 +1,79 @@
+from collections import defaultdict
+from typing import Dict, List, Tuple
+
+import pronouncing
+
+
+class RhymeBuilder:
+    def __init__(self) -> None:
+        pass
+
+    def count_word_syllables(self, word: str) -> int:
+        pronunciations = pronouncing.phones_for_word(word)
+
+        if not pronunciations:
+            return 1
+
+        syllable_count = pronouncing.syllable_count(pronunciations[0])
+        return syllable_count
+
+    def create_syllable_count_dict(self, file_lines: List[str]):
+        syllable_count_dict = defaultdict(list)
+
+        for line_number, line in enumerate(file_lines):
+            total_syllables = 0
+            word_index = len(line) - 1
+
+            for word in reversed(line):
+                syllable_count = self.count_word_syllables(word)
+
+                syllable_count_dict[word].append(
+                    (
+                        line_number,
+                        word_index,
+                        total_syllables,
+                        total_syllables + syllable_count,
+                    )
+                )
+
+                total_syllables += syllable_count
+                word_index -= 1
+
+        return syllable_count_dict
+
+    def iterate_over_dict(
+        self,
+        syllable_count_dict: Dict[Tuple[int], str],
+        all_matched_word_placements: List[Tuple[int]],
+    ):
+        main_word = next(iter(syllable_count_dict))
+        main_word_placements = syllable_count_dict[main_word]
+        words_to_delete = []
+        combined_word_placements = []
+
+        del syllable_count_dict[main_word]
+
+        rhymes = pronouncing.rhymes(main_word)
+
+        for word, word_placements in syllable_count_dict.items():
+            if word in rhymes:
+                combined_word_placements.extend(word_placements)
+                words_to_delete.append(word)
+
+        for word in words_to_delete:
+            del syllable_count_dict[word]
+
+        if combined_word_placements:
+            combined_word_placements.extend(main_word_placements)
+            all_matched_word_placements.append(combined_word_placements)
+
+        if syllable_count_dict:
+            self.iterate_over_dict(syllable_count_dict, all_matched_word_placements)
+
+    def get_matched_word_placements(self, file_lines: List[str]) -> List[List[Tuple[int]]]:
+        matched_word_placements = []
+
+        syllable_count_dict = self.create_syllable_count_dict(file_lines)
+        self.iterate_over_dict(syllable_count_dict, matched_word_placements)
+
+        return matched_word_placements
